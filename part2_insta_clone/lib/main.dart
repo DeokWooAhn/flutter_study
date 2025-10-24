8f742549ce0bee76d101516b4c808145fed81e29
DeokWooAhn <fjrlvkdlxj@naver.com>
2025-10-24 13:30:16 +0900

    홈 화면 만드는중

--- part2_insta_clone/lib/main.dart
+++ part2_insta_clone/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'body.dart';
 
 void main() {
   runApp(const InstaCloneApp());
@@ -9,8 +13,81 @@ class InstaCloneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Colors.white,
+          secondary: Colors.black,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.black,
+        ),
+        useMaterial3: true,
+      ),
+      home: const InstaCloneHome(),
+    );
   }
 }
 
+class InstaCloneHome extends StatefulWidget {
+  const InstaCloneHome({super.key});
 
+  @override
+  State<InstaCloneHome> createState() => _InstaCloneHomeState();
+}
+
+class _InstaCloneHomeState extends State<InstaCloneHome> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Instagram",
+          style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.favorite_outline, size: 32),
+            onPressed: () {
+              print("tab");
+            },
+          ),
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, size: 32),
+            onPressed: () {
+              print("tab");
+            },
+          ),
+        ],
+      ),
+      body: InstaBody(index: index),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (newIndex) {
+          setState(() => index = newIndex);
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 28),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 28),
+            label: "Search",
+          ),
+        ],
+      ),
+    );
+  }
+}
