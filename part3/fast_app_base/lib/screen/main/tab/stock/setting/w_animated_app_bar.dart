0b44a3e918d86a78ddc72145115f1411ad91c53f
DeokWooAhn <fjrlvkdlxj@naver.com>
2026-03-08 23:57:52 +0900

    커스텀 implict Animation

--- part3/fast_app_base/lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
+++ part3/fast_app_base/lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
@@ -12,9 +12,13 @@ class AnimatedAppBar extends StatefulWidget {
   State<AnimatedAppBar> createState() => _AnimatedAppBarState();
 }
 
-class _AnimatedAppBarState extends State<AnimatedAppBar> {
+class _AnimatedAppBarState extends State<AnimatedAppBar>
+    with SingleTickerProviderStateMixin {
   Duration get duration => 10.ms;
   double scrollPosition = 0;
+  late final controller = AnimationController(vsync: this);
+  late Animation animation =
+      CurvedAnimation(parent: controller, curve: Curves.bounceInOut);
 
   @override
   void initState() {
@@ -29,7 +33,7 @@ class _AnimatedAppBarState extends State<AnimatedAppBar> {
   bool get isTriggered => scrollPosition > 80;
 
   double getValue(double initial, double target) {
-    if(isTriggered) {
+    if (isTriggered) {
       return target;
     }
     double fraction = scrollPosition / 80;
@@ -53,16 +57,40 @@ class _AnimatedAppBarState extends State<AnimatedAppBar> {
               ),
             ).p20(),
             AnimatedContainer(
-                duration: duration,
-                padding: EdgeInsets.only(
-                    left: getValue(20, 50),
-                    top: getValue(20, 50)),
-                child: AnimatedDefaultTextStyle(
-                    child: widget.title.text.make(),
-                    style: TextStyle(
-                        fontSize: isTriggered ? 30 : 18,
-                        fontWeight: FontWeight.bold),
-                    duration: duration)),
+              duration: duration,
+              padding: EdgeInsets.only(
+                  left: getValue(20, 50), top: getValue(20, 50)),
+              child: AnimatedDefaultTextStyle(
+                  child: widget.title.text.make(),
+                  style: TextStyle(
+                      fontSize: isTriggered ? 30 : 18,
+                      fontWeight: FontWeight.bold),
+                  duration: duration),
+            ),
+            Tap(
+                onTap: () {
+                  Nav.pop(context);
+                },
+                child: const Arrow(
+                  direction: AxisDirection.left,
+                )).p20(),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.topRight,
+                child: TweenAnimationBuilder<Color?>(
+                  duration: 1000.ms,
+                  tween: ColorTween(
+                      begin: Colors.green,
+                      end: isTriggered ? Colors.orange : Colors.green),
+                  builder: (context, value, child) => Image.asset(
+                    "$basePath/icon/map_point.png",
+                    height: 60,
+                    color: value,
+                    colorBlendMode: BlendMode.modulate,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
